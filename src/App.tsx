eb1fa43572a80bfd60f321f067199d07c2eae29f
ValentinaPaglinoAdv <valentina.paglino@adviters.com>
2025-02-21 18:06:37 -0300

    npm fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // src/App.tsx
 import React from "react";
-import "./App.css";
 import Canvas from "./components/Canvas/CustomCanvas";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Sidebar />
-        <Canvas />
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Sidebar />
+          <Canvas />
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "index.ts", // El archivo principal de tu librería
+      name: "react-templates-dnd", // El nombre de la librería
+      fileName: (format) => `react-templates-dnd.${format}.js`,
+    },
+    rollupOptions: {
+      // Asegúrate de que no haya conflicto con dependencias externas
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
